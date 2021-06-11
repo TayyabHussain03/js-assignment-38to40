a95da0a26a9d53bd9f50015ad7df87848aeda485
TayyabHussain <tayyabfaisal106@gmail.com>
2021-06-11 18:49:10 +0500

    question2 added

--- app.js
+++ app.js
@@ -5,3 +5,20 @@
 //  console.log(res);
 // }
 // pow(5,2);
+
+
+// **********QUESTION 2**********
+
+// var input = prompt("Enter a year and see that whther the year is leap year or not")
+// function leapYear(){
+//     if (input % 4 == 0 && input % 100!=0) {
+//     alert(input + " is a Leap year");}
+
+//     else if (input % 100 == 0 && input % 400 == 0){
+//     alert(input + " is a Leap year");}
+    
+//     else {
+//     alert(input + " is not a Leap year");
+//     }
+//     };
+//     leapYear()
